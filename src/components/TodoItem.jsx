9240d52b42f77d8cc28a49f94ae15724ee21e0e2
rosemutai <chepngetichrose2030@gmail.com>
2023-05-01 22:14:55 +0300

    Add a single todo component

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable react/prop-types */
+
+const TodoItem = ({ itemProp, handleChange, deleteTodo }) => {
+  return (
+    <li>
+      <input
+        type="checkbox"
+        checked={itemProp.completed}
+        onChange={() => handleChange(itemProp.id)}
+      />
+      <button
+        onClick={() => deleteTodo(itemProp.id)}
+      >
+        Delete
+      </button>
+      {itemProp.title}
+    </li>
+  )
+  
+}
+
+export default TodoItem

34313d97760fbab98b5146e96bd5b8081b510d9c
rosemutai <chepngetichrose2030@gmail.com>
2023-05-02 08:32:39 +0300

    Add About route

---
+++ src/routes/About.jsx
@@ -0,0 +1,19 @@
+import { NavLink, Outlet } from "react-router-dom";
+
+const About = () => {
+  return (
+    <div className="about">
+      <ul className="about_list">
+        <li>
+          <NavLink to="about-app">About app</NavLink>
+        </li>
+        <li>
+          <NavLink to="about-developer">About developer</NavLink>
+        </li>
+      </ul>
+      <Outlet />
+    </div>
+  );
+}
+
+export default About
